bf5fa44702d656192e95ff08723f6e8b9465af5a
Quang Nguyen <vuquangkhtn@gmail.com>
2022-01-04 22:50:26 +0700

    Implement download API

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var cors = require('cors');
 var { graphqlHTTP } = require('express-graphql');
 const { makeExecutableSchema } = require('graphql-tools');
 
@@ -12,8 +13,14 @@ const executableSchema = makeExecutableSchema({
 })
 
 var app = express();
+app.use(cors());
+
 app.use('/graphql', graphqlHTTP({
   schema: executableSchema,
   graphiql: true,
 }));
+app.get('/download-resume', function(req, res){
+  const file = `${__dirname}/data/CV_NguyenVuQuang.pdf`;
+  res.download(file, 'CV_NguyenVuQuang.pdf');
+});
 app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
